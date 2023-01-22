7b3b4e411a1e98a970def731b0895d5206e209dc
yocajii <yocajii@stone.sakura.ne.jp>
2023-01-22 04:32:29 +0000

    2.3.9 暗黙の変換を体験する

---
+++ src/type-conversion.ts
@@ -0,0 +1,12 @@
+import { createInterface } from 'readline';
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.question('文字列を入力してください:', (line) => {
+  console.log(`${line}が入力されました`);
+  console.log(line + 1000);
+  rl.close();
+});
